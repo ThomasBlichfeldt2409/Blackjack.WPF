f41967389ea00702dcaa65ca9a8c48430750cac6
TheSharpKillers <thomasbn2409@gmail.com>
2026-02-17 09:52:28 +0100

    NavigationPanel
    Added background and started the Navigation Panel

---
+++ Blackjack.WPF/Blackjack.WPF/Views/NavigationPanel.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Blackjack.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for NavigationPanel.xaml
+    /// </summary>
+    public partial class NavigationPanel : UserControl
+    {
+        public NavigationPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
