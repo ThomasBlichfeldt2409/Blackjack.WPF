83a2691bb00e96bec654973c77f12132b0225a88
ThomasBlichfeldt2409 <Thomas.Blichfeldt@specialisterne.dk>
2026-02-18 13:48:50 +0100

    PlayerRepository
    Created PlayerRepository in the Data project.
    Override the OnStartup method in app.xaml.cs

---
+++ Blackjack.WPF/Blackjack.Data/PlayerRepository.cs
@@ -0,0 +1,20 @@
+﻿using Blackjack.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blackjack.Data
+{
+    public class PlayerRepository
+    {
+        private readonly string? _databasePath;
+
+        public PlayerRepository(string databasePath)
+        {
+            _databasePath = databasePath; 
+        }
+
+        private BlackjackDbContext CreateContext()
+        {
+            return new BlackjackDbContext(_databasePath!);
+        }
+    }
+}
--- Blackjack.WPF/Blackjack.WPF/App.xaml.cs
+++ Blackjack.WPF/Blackjack.WPF/App.xaml.cs
@@ -1,14 +1,26 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Blackjack.Data;
+using System.IO;
 using System.Windows;
 
 namespace Blackjack.WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        public static string DbPath { get; private set; } = string.Empty;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Set the database path
+            string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Blackjack");
+            Directory.CreateDirectory(folder);
+
+            DbPath = Path.Combine(folder, "blackjack.db");
+
+            // Creating / Connecting to the Database
+            using BlackjackDbContext context = new BlackjackDbContext(DbPath);
+            context.Database.EnsureCreated();
+        }
+    }
 }
