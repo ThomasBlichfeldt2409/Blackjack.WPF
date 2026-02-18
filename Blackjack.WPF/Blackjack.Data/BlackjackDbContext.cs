37d701e9fb71c63420f1ab92342d7f4699bf7de7
ThomasBlichfeldt2409 <Thomas.Blichfeldt@specialisterne.dk>
2026-02-18 12:02:38 +0100

    Created BlackjackDbContext
    Added BlackjackDbContext to the Data project.
    
    Installed:
    Microsoft.EntityFrameworkCore and Microsoft.EntityFrameworkCore.Sqlite to the Data project.

---
+++ Blackjack.WPF/Blackjack.Data/BlackjackDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Blackjack.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blackjack.Data
+{
+    public class BlackjackDbContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+
+        private readonly string? _databasePath;
+
+        public BlackjackDbContext(string databasePath)
+        {
+            _databasePath = databasePath;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={_databasePath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Player>(entity =>
+            {
+                entity.HasKey(p => p.Name);
+                entity.Property(p => p.Name).IsRequired();
+                entity.Property(p => p.Bank).IsRequired();
+                entity.Property(p => p.BiggestWin).IsRequired();
+                entity.Property(p => p.BiggestLoose).IsRequired();
+                entity.Property(p => p.Total).IsRequired();
+            });
+        }
+    }
+}
--- Blackjack.WPF/Blackjack.Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Blackjack.Data
-{
-    public class Class1
-    {
-
-    }
-}
